e208999e0bb4c5eb165579ca48e70020d7bd0888
Sravani Nalla <52592618+sravani1119@users.noreply.github.com>
2023-06-30 17:00:11 -0500

    Solution for assignment 07
    Solution for assignment 07

---
+++ sravani/Assig07.java
@@ -0,0 +1,37 @@
+package assignments;
+
+public class Assig07 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		// using for loop
+
+	/*	int num = 5;
+		int factorial = 1;
+		for (int i = 1; i <= num; i++) {
+
+			factorial = i * factorial;
+
+		}
+
+		System.out.println(factorial);
+
+	}*/
+
+		// using while loop
+		
+		int num = 5;
+		int factorial = 1;
+		int i= 1;
+		while (i<=num) {
+
+			factorial = i * factorial;
+			i++;
+
+		}
+
+		System.out.println(factorial);
+
+	}
+}
