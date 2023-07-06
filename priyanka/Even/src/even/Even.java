609013e8ae7dd92240e6c4a995f252e7c47bfd79
pashepu <priya082419@gmail.com>
2023-07-05 23:38:14 -0400

    Assignment 7

---
+++ priyanka/Even/src/even/Even.java
@@ -0,0 +1,19 @@
+package even;
+
+public class Even {
+
+	public static void main(String[] args)
+	{
+		int num=100;
+		System.out.print("Even numbers from 1 to" +num+":");
+		for(int i=1; i<=num; i++) 
+		{ //to check if the number is even or not 
+			if(i%2==0) {
+				System.out.print(i + "");
+			}
+		}
+		
+
+	}
+
+}
