4e1c4c66bf74ecb995812875f44c82c583906582
pashepu <priya082419@gmail.com>
2023-07-05 23:31:41 -0400

    Assignment 8

---
+++ priyanka/Arrays/src/arrays/Arrays.java
@@ -0,0 +1,18 @@
+package arrays;
+
+public class Arrays {
+
+	public static void main(String[] args) {
+		int[] a1 = new int[] {2, 3, 4, 5};
+		int[] a2 = new int[] {2, 3, 4, 5};
+		if(a1 == a2) { 
+		
+       System.out.println("true");
+		}
+       else {
+	   System.out.println("false");
+       }	
+			
+}
+}
+  
