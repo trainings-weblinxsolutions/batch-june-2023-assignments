131b59e0e817150e518a97d1071352ebddd733cb
PrudhviStonzz <prudhvichappidi@gmail.com>
2023-07-13 14:30:53 -0400

    assignment5

---
+++ prudhvi/NumbertoBinary.java
@@ -0,0 +1,13 @@
+//Assignment5
+public class NumbertoBinary {
+    public static void main(String arg[]) {
+
+        int num = 9;
+        String bin = Integer.toBinaryString(num);
+        System.out.println(bin);
+
+        String binary = String.valueOf(101011);
+        int decimal = Integer.parseInt(binary, 2);
+        System.out.println(decimal);
+    }
+}
