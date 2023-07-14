ef84d3a54ccf35e1c6d8751da45f95b6e3039395
PrudhviStonzz <prudhvichappidi@gmail.com>
2023-07-13 21:45:05 -0400

    assignment7

---
+++ prudhvi/Evennumbers.java
@@ -0,0 +1,11 @@
+public class Evennumbers {
+    public static void main(String[] args) {
+
+        int num = 100;
+        for (int i=1; i<=num; i++) {
+            if (i % 2 == 0)
+                System.out.print(i + " ");
+        }
+}
+}
+
---
+++ prudhvi/Factorialofanumber.java
@@ -0,0 +1,14 @@
+public class Factorialofanumber {
+    public static void main(String[] args) {
+
+        int num = 4;
+        int factorial = 1 ;
+        int i = 1;
+        while(i<=num)
+        {
+            factorial = factorial * i;
+            i++;
+        }
+        System.out.println("Factorial = " + factorial);
+}
+}
