f67565cc8588848a1b43c750d003255875b8fb37
PrudhviStonzz <prudhvichappidi@gmail.com>
2023-07-13 21:58:26 -0400

    assignment10

--- praveen/instructions.md
+++
@@ -1,3 +0,0 @@
-### This is your folder to checkin the code for assignments.
-#### Please only use your folder when you checkin the code for your assignments.
-#### Do not delete or modify files from other students folders.
---
+++ prudhvi/Printnumbers.java
@@ -0,0 +1,11 @@
+public class Printnumbers {
+    public static void main(String[] args) {
+
+        int num = 100;
+        for (int i=2; i<=num; i++) {
+            if (i <= 100)
+                System.out.print(i + " ");
+        }
+}
+}
+
